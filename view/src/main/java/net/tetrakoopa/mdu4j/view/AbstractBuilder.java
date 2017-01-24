837833878df966828c89704c8c88a003a7ea4653
Martin Salazard <martin.der.aze@gmail.com>
2017-01-24 22:53:14 +0100

    View : some basic pojo parsing working

--- view/src/main/java/net/tetrakoopa/mdu4j/view/AbstractBuilder.java
+++ view/src/main/java/net/tetrakoopa/mdu4j/view/AbstractBuilder.java
@@ -5,6 +5,7 @@ import net.tetrakoopa.mdu4j.util.ReflectionUtil;
 
 import org.springframework.util.ReflectionUtils;
 
+import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
@@ -23,7 +24,8 @@ public abstract class AbstractBuilder<CONTEXT> implements UIAttribute {
 		BOOLEAN,
 		OBJECT,
 		ONE_IN_MANY,
-		MANY_IN_MANY
+		MANY_IN_MANY,
+		ARRAY,
 	}
 	
 	/** Type Spécific view choice */
@@ -53,7 +55,37 @@ public abstract class AbstractBuilder<CONTEXT> implements UIAttribute {
 
 	public static class ViewElement {
 
-		public final Field field;
+		public static class InternalReference {
+			final Field field;
+			final int index;
+			final String key;
+
+			InternalReference(Field field) {
+				this.field = field;
+				this.index = 0;
+				this.key = null;
+			}
+			InternalReference(int index) {
+				this.field = null;
+				this.index = index;
+				this.key = null;
+			}
+			InternalReference(String key) {
+				this.field = null;
+				this.index = 0;
+				this.key = key;
+			}
+			@Override
+			public String toString() {
+				if (field != null)
+					return field.getName();
+				if (key != null)
+					return key;
+				return String.valueOf(index);
+			}
+		}
+
+		final InternalReference internalReference;
 
 		public final Class<?> clazz;
 		public final ViewElement parent;
@@ -61,58 +93,83 @@ public abstract class AbstractBuilder<CONTEXT> implements UIAttribute {
 		public final String label;
 		public final Type type;
 
-		public ViewElement(Field field, String label, Type type, Class<?> clazz, ViewElement parent) {
-			this.field = field;
+		public final Object value;
+
+		public ViewElement(InternalReference internalReference, String label, Type type, Class<?> clazz, Object value, ViewElement parent) {
+			this.internalReference = internalReference;
 			this.label = label;
-			this.type = type;
 			this.clazz = clazz;
+			this.type = type;
+			this.value = value;
 			this.parent = parent;
 		}
+
 	}
 
 	private static class BuilderContext {
 	}
 
-	private <BEAN> void build(final BuilderContext builderContext, final CONTEXT context, final List<ViewElement> elements, final ViewElement parent,final BEAN bean) {
-		final Class<?> clazz = bean.getClass();
-		final PojoPolicy policy =  clazz.getAnnotation(PojoPolicy.class);
+	private <BEAN> void build(final BuilderContext builderContext, final List<ViewElement> elements, Class<?> clazz, ViewElement.InternalReference internalReference, final ViewElement parent,final BEAN bean) {
 
-		final boolean partakeByDefault = policy == null || (policy.fieldsPartaking()==FieldPartaking.INCLUDE);
+		final Type type = infereType(clazz);
 
-		ReflectionUtils.doWithFields(clazz, new ReflectionUtils.FieldCallback() {
-			@Override
-			public void doWith(Field field) throws IllegalArgumentException, IllegalAccessException {
-				final Partaking partaking = field.getAnnotation(Partaking.class);
-				if (!partakeByDefault) {
-					if(partaking == null || partaking.value() == FieldPartaking.EXCLUDE) {
-						return;
-					}
-				} else {
-					if (partaking != null && partaking.value() == FieldPartaking.EXCLUDE) {
-						return;
-					}
-				}
-				final Label labelAnnotation = field.getAnnotation(Label.class);
-				final String label = labelAnnotation == null ? field.getName() : labelAnnotation.value();
-				final Type type = infereType(field);
-				final ViewElement element = new ViewElement(field, label, type, clazz, parent);
-				if (type == Type.OBJECT) {
-					ReflectionUtils.makeAccessible(field);
-					build(builderContext, context, elements, element, field.get(bean));
-				} else {
-					elements.add(element);
-				}
+		final String label;
+		if (internalReference.field != null) {
+			final Label labelAnnotation = internalReference.field.getAnnotation(Label.class);
+			label = labelAnnotation == null ? internalReference.field.getName() : labelAnnotation.value();
+		} else if (internalReference.key != null) {
+			label = internalReference.key;
+		} else {
+			label = String.valueOf(internalReference.index);
+		}
+
+		final ViewElement element = new ViewElement(internalReference, label, type, clazz, bean, parent);
+
+		elements.add(element);
+
+		if (bean == null)
+			return;
+		if (type == Type.ARRAY) {
+			final int length = Array.getLength(bean);
+			for (int index = 0; index < length; index ++) {
+				final Object arrayElement = Array.get(bean, index);
+				build(builderContext, elements, clazz.getComponentType(), new ViewElement.InternalReference(index), element, arrayElement);
 			}
-		});
+			return;
+		}
+
+		if (type == Type.OBJECT) {
+			final PojoPolicy policy =  clazz.getAnnotation(PojoPolicy.class);
+			final boolean partakeByDefault = policy == null || (policy.fieldsPartaking()==FieldPartaking.INCLUDE);
+			ReflectionUtils.doWithFields(clazz, new ReflectionUtils.FieldCallback() {
+				@Override
+				public void doWith(Field field) throws IllegalArgumentException, IllegalAccessException {
+					ReflectionUtils.makeAccessible(field);
+					final Partaking partaking = field.getAnnotation(Partaking.class);
+					if (!partakeByDefault) {
+						if(partaking == null || partaking.value() == FieldPartaking.EXCLUDE) {
+							return;
+						}
+					} else {
+						if (partaking != null && partaking.value() == FieldPartaking.EXCLUDE) {
+							return;
+						}
+					}
+					build(builderContext, elements, field.getType(), new ViewElement.InternalReference(field), element, field.get(bean));
+				}
+			});
+			return;
+		}
 	}
 
-	private Type infereType(Field field) {
-		final Class<?> fieldType = field.getType();
-		if (fieldType.equals(String.class))
+	private Type infereType(Class<?> clazz) {
+		if (clazz.isArray())
+			return Type.ARRAY;
+		if (clazz.equals(String.class))
 			return Type.TEXT;
-		if (fieldType.equals(Integer.class) || fieldType.equals(Long.class))
+		if (clazz.equals(Integer.class) || clazz.equals(Integer.TYPE) || clazz.equals(Long.class) || clazz.equals(Long.TYPE))
 			return Type.NUMBER;
-		if (fieldType.equals(Float.class) || fieldType.equals(Double.class))
+		if (clazz.equals(Float.class) || clazz.equals(Float.TYPE) || clazz.equals(Double.class) || clazz.equals(Double.TYPE))
 			return Type.NUMBER;
 		return Type.OBJECT;
 	}
@@ -123,7 +180,21 @@ public abstract class AbstractBuilder<CONTEXT> implements UIAttribute {
 
 		final BuilderContext builderContext = new BuilderContext();
 
-		build(builderContext, context, elements, null, bean);
+		final Class<?> clazz = bean.getClass();
+ 		final Type type = infereType(clazz);
+
+		if (type != Type.OBJECT) {
+			throw new IllegalArgumentException("Argument 'bean' must be a POJO ( provided '"+bean.getClass().getName()+"' )");
+		}
+
+
+		ReflectionUtils.doWithFields(clazz, new ReflectionUtils.FieldCallback() {
+			@Override
+			public void doWith(Field field) throws IllegalArgumentException, IllegalAccessException {
+				ReflectionUtils.makeAccessible(field);
+				build(builderContext, elements, field.getType(), new ViewElement.InternalReference(field),null,field.get(bean));
+			}
+		});
 
 		for (ViewElement element : elements) {
 			buildForField(context, element);
